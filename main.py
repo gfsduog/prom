927bac858e8afbee6d6ac04b30031a18417129dd
Nickolay Smirnov <Nickolay.Smirnov@hotmail.co.uk>
2016-04-21 13:29:30 +0100

    lol

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from serial import Serial
 from smbus import SMBus
+import RPi.GPIO as gpio
 from time import time
-import random
 
 BLACK = 0
 NET = RED = 1
@@ -34,12 +34,12 @@ Player0Score = 0
 Player1Score = 0
 Player0Bat = 0
 Player1Bat = 0
-Player0Height = 10
-Player1Height = 10
+Player0Height = 3
+Player1Height = 3
 ServeCount = 0
 Serving = True
-BallX = WIDTH/2
-BallY = HEIGHT/2
+BallX = WIDTH / 2
+BallY = HEIGHT / 2
 BallXSpeed = 1
 BallYSpeed = 1
 
@@ -47,8 +47,8 @@ def bugger():
     bugger = WIDTH * HEIGHT * [BLACK]
 
     for i in range(2, HEIGHT - 1, 4):
-        bugger[WIDTH / 2 + WIDTH * i] = NET
-        bugger[WIDTH / 2 + WIDTH * (i + 1)] = NET
+        bugger[WIDTH / 2 + i * WIDTH] = NET
+        bugger[WIDTH / 2 + (i + 1) * WIDTH] = NET
     
     for i in SCORES[Player0Score]:
         bugger[WIDTH / 2 - 10 + i] = SCORE
@@ -56,11 +56,11 @@ def bugger():
         bugger[WIDTH / 2 + 8 + i] = SCORE
 
     for i in range(Player0Height):
-        bugger[2 + WIDTH * (Player0Bat + i)] = BAT
+        bugger[2 + (Player0Bat + i) * WIDTH] = BAT
     for i in range(Player1Height):
-        bugger[WIDTH - 3 + WIDTH * (Player1Bat + i)] = BAT
+        bugger[WIDTH - 3 + (Player1Bat + i) * WIDTH] = BAT
 
-    bugger[ BallY * WIDTH + BallX ] = BALL
+    bugger[BallX + BallY * WIDTH] = BALL
 
     return bugger
 
@@ -80,41 +80,48 @@ def output():
                     write('\033[' + str(i / WIDTH + 1) + ';' + str(i % WIDTH + 1) + 'H')
                 write(' ')
 
-def point_won():
-    global Player1Score, Player0Score, BallX, BallY, ServeCount, BallXSpeed
-    if BallX == 0: Player1Score += 1
-    else: Player0Score += 1
-    ServeCount = (ServeCount+1)%10
-    if ServeCount < 5: BallXSpeed = 1
-    else: BallXSpeed = -1
-    BallX = WIDTH/2
-    BallY = HEIGHT/2
-
 with Serial('/dev/ttyAMA0', 115200) as cereal:
     adc = SMBus(1)
     write = cereal.write
     write('\033[?25l')
+    gpio.setmode(gpio.BCM)
+    gpio.setup(7, gpio.IN, gpio.PUD_UP)
+    gpio.setup(8, gpio.IN, gpio.PUD_UP)
+    gpio.setup(9, gpio.IN, gpio.PUD_UP)
+    gpio.setup(10, gpio.IN, gpio.PUD_UP)
+    gpio.setwarnings(False)
     oldBugger = WIDTH * HEIGHT * [None]
     oldTime = time()
     while True:
         newTime = time()
-        #print 1 / (newTime - oldTime)
-        adc.write_byte(33, 128)
+        adc.write_byte(33, 64)
         knob = adc.read_word_data(33, 0)
-        Player0Bat = int(round(((((knob & 15) << 8) | (knob >> 8)) / 4096.) * (HEIGHT - Player0Height)))
-        adc.write_byte(33, 0x40)
+        Player0Bat = HEIGHT - int(round(((knob & 15) << 8 | knob >> 8) * (HEIGHT - Player0Height) / 4096.)) - Player0Height
+        adc.write_byte(33, 16)
         knob = adc.read_word_data(33, 0)
-        Player1Bat = int(round(((((knob & 15) << 8) | (knob >> 8)) / 4096.) * (HEIGHT - Player1Height)))
-        if not Serving:
+        Player1Bat = HEIGHT - int(round(((knob & 15) << 8 | knob >> 8) * (HEIGHT - Player1Height) / 4096.)) - Player1Height
+        if Serving:
+            if ServeCount < 5:
+                BallY = Player0Bat+(Player0Height//2)
+                BallX = 3
+            else:
+                BallY = Player1Bat+(Player1Height//2)
+                BallX = WIDTH - 4
+        else:
             BallX += BallXSpeed
             BallY += BallYSpeed
-            if BallX == 0 or BallX == WIDTH-1: point_won()
-            if BallY == 0 or BallY == HEIGHT-1: BallYSpeed = -BallYSpeed
-            if BallX == 3 and BallY in range(Player0Bat, Player0Bat+Player0Height): BallXSpeed = -BallXSpeed
-            if BallX == WIDTH-4 and BallY in range(Player1Bat, Player1Bat+Player1Height): BallXSpeed = -BallXSpeed
-        else:
-            if ServeCount < 5: BallY = Player0Bat+(Player0Height//2)
-            else: BallY = Player1Bat+(Player1Height//2)
+            if BallX == 0 or BallX == WIDTH - 1:
+                ServeCount = (ServeCount+1)%10
+                Serving = True
+                if BallX == 0: Player1Score += 1
+                else: Player0Score += 1
+                if ServeCount < 5: BallXSpeed = 1
+                else: BallXSpeed = -1
+            if BallY == 0 or BallY == HEIGHT - 1: BallYSpeed = -BallYSpeed
+            if BallX == 3 and BallY in range(Player0Bat, Player0Bat + Player0Height): BallXSpeed = -BallXSpeed
+            if BallX == WIDTH-4 and BallY in range(Player1Bat, Player1Bat + Player1Height): BallXSpeed = -BallXSpeed
+        if (not gpio.input(10) and ServeCount < 5) or (not gpio.input(9) and ServeCount >= 5):
+            Serving = False
         currentBugger = bugger()
         output()
         oldBugger = currentBugger
