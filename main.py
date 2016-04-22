4e84400a8c2d7ae903d0f409408902e527000918
Nickolay Smirnov <Nickolay.Smirnov@hotmail.co.uk>
2016-04-22 17:26:31 +0100

    Added lots of features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from serial import Serial
 from smbus import SMBus
 import RPi.GPIO as gpio
 from time import time
+from random import uniform
 
 BLACK = 0
 NET = RED = 1
@@ -11,6 +12,8 @@ BLUE = 4
 MAGENTA = 5
 BAT = CYAN = 6
 BALL = YELLOW = 7
+
+LEDS = (5, 6, 12, 13, 16, 19, 20, 26)
 
 COLOURS = ('\033[40m', '\033[41m', '\033[42m', '\033[47m', '\033[44m', '\033[45m', '\033[46m', '\033[43m')
 
@@ -30,6 +33,8 @@ SCORES = (
 (WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2, WIDTH * 2 + 2, WIDTH * 3, WIDTH * 3 + 1, WIDTH * 3 + 2, WIDTH * 4 + 2, WIDTH * 5 + 2),
 )
 
+BASE_SPEED = 8. / WIDTH
+
 Player0Score = 0
 Player1Score = 0
 Player0Bat = 0
@@ -38,10 +43,13 @@ Player0Height = 3
 Player1Height = 3
 ServeCount = 0
 Serving = True
-BallX = WIDTH / 2
-BallY = HEIGHT / 2
 BallXSpeed = 1
 BallYSpeed = 1
+Player0SizeCounter = 15
+Player0Megabats = 2
+Player1SizeCounter = 15
+Player1Megabats = 2
+
 
 def bugger():
     bugger = WIDTH * HEIGHT * [BLACK]
@@ -65,7 +73,7 @@ def bugger():
     return bugger
 
 def output():
-    delta = [[] for i in range(len(COLOURS))]
+    delta = [[] for i in COLOURS]
     for i in range(WIDTH * HEIGHT):
         if oldBugger[i] != currentBugger[i]:
             delta[currentBugger[i]].append(i)
@@ -80,26 +88,46 @@ def output():
                     write('\033[' + str(i / WIDTH + 1) + ';' + str(i % WIDTH + 1) + 'H')
                 write(' ')
 
+def megabat(port):
+    global Player0Megabats, Player0Height, Player1Megabats, Player1Height, Player0SizeCounter, Player1SizeCounter
+    if port == 9 and Player0Megabats:
+        Player0Height <<= 1
+        Player0Megabats -= 1
+        Player0SizeCounter = 0
+    elif port == 4 and Player1Megabats:
+        Player1Height <<= 1
+        Player1Megabats -= 1
+        Player1SizeCounter = 0
+
 with Serial('/dev/ttyAMA0', 115200) as cereal:
     adc = SMBus(1)
     write = cereal.write
     write('\033[?25l')
     gpio.setwarnings(False)
     gpio.setmode(gpio.BCM)
-    gpio.setup(8, gpio.IN, gpio.PUD_UP)
+    gpio.setup(4, gpio.IN, gpio.PUD_UP)
     gpio.setup(9, gpio.IN, gpio.PUD_UP)
+    gpio.add_event_detect(4, gpio.FALLING, megabat, 15000)
+    gpio.add_event_detect(9, gpio.FALLING, megabat, 15000)
     gpio.setup(10, gpio.IN, gpio.PUD_UP)
     gpio.setup(11, gpio.IN, gpio.PUD_UP)
+    for led in LEDS: gpio.setup(led, gpio.OUT)
     oldBugger = WIDTH * HEIGHT * [None]
     oldTime = time()
+    counter = 0
     while 1:
         newTime = time()
+        counter += newTime - oldTime
         adc.write_byte(33, 128)
         knob = adc.read_word_data(33, 0)
         Player0Bat = HEIGHT - int(round(((knob & 15) << 8 | knob >> 8) * (HEIGHT - Player0Height) / 4096.)) - Player0Height
         adc.write_byte(33, 16)
         knob = adc.read_word_data(33, 0)
         Player1Bat = HEIGHT - int(round(((knob & 15) << 8 | knob >> 8) * (HEIGHT - Player1Height) / 4096.)) - Player1Height
+        if Player0SizeCounter < 15: Player0SizeCounter += newTime - oldTime
+        else: Player0Height = 3
+        if Player1SizeCounter < 15: Player1SizeCounter += newTime - oldTime
+        else: Player1Height = 3
         if Serving:
             if ServeCount < 5:
                 BallY = Player0Bat + Player0Height / 2
@@ -108,20 +136,34 @@ with Serial('/dev/ttyAMA0', 115200) as cereal:
                 BallY = Player1Bat + Player1Height / 2
                 BallX = WIDTH - 4
             Serving = gpio.input(10) and ServeCount < 5 or gpio.input(11) and ServeCount >= 5
+            if not Serving:
+                threshold = BASE_SPEED * uniform(.5, 1.5)
+                counter = 0
         else:
-            BallX += BallXSpeed
-            BallY += BallYSpeed
-            if BallX == 0 or BallX == WIDTH - 1:
-                Serving = True
-                ServeCount = (ServeCount + 1) % 10
-                if BallX == 0: Player1Score += 1
-                else: Player0Score += 1
-                if ServeCount < 5: BallXSpeed = 1
-                else: BallXSpeed = -1
-            if BallY == 0 or BallY == HEIGHT - 1: BallYSpeed = -BallYSpeed
-            if BallX == 2 and 0 < BallY - Player0Bat < Player0Height or BallX == WIDTH - 3 and 0 < BallY - Player1Bat < Player1Height:
-                BallXSpeed = -BallXSpeed
+            while counter >= threshold:
+                counter -= threshold
                 BallX += BallXSpeed
+                BallY += BallYSpeed
+                if BallX == 0 or BallX == WIDTH - 1:
+                    Serving = True
+                    ServeCount = (ServeCount + 1) % 10
+                    if BallX == 0: Player1Score += 1
+                    else: Player0Score += 1
+                    BallXSpeed = 1 if ServeCount < 5 else -1
+                if BallX == 2 and 0 <= BallY - Player0Bat < Player0Height or BallX == WIDTH - 3 and 0 <= BallY - Player1Bat < Player1Height:
+                    BallXSpeed = -BallXSpeed
+                    BallX += BallXSpeed
+                    if BallX == 3:
+                        BallYSpeed = 3 * (BallY - Player0Bat) / Player0Height - 1
+                    else:
+                        BallYSpeed = 3 * (BallY - Player1Bat) / Player1Height - 1
+                    threshold = BASE_SPEED * uniform(.5, 1.5)
+                if BallY == -1 or BallY == HEIGHT:
+                    BallYSpeed = -BallYSpeed
+                    BallY += BallYSpeed
+        for led in LEDS: gpio.output(led, False)
+        gpio.output(LEDS[8 * BallX / (WIDTH + 1)], True)
+        print(Player0Megabats, Player1Megabats)
         currentBugger = bugger()
         output()
         oldBugger = currentBugger
